c1506137210be8e09c4cc2a9bc96ab9db66969db
Salmaan <salmaan.arif.ali@gmail.com>
2020-12-15 15:09:36 -0500

    Done.

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -6,7 +6,7 @@ def fifth_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "dead"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
@@ -15,7 +15,7 @@ def fifth_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "dead"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
@@ -24,7 +24,8 @@ def fifth_challenge
   }
 
   #code your solution here:
-
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The method must return our newly modified epic tragedy hash
   epic_tragedy
